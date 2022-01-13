cc002b06b353c4effd8d4adfa18a0c0d5fef9d3c
Jason Bullard <jason@bullard.io>
2022-01-12 19:24:59 -0700

    Add RecipeTabView as main entry point

---
+++ Recipe App/Views/RecipeTabView.swift
@@ -0,0 +1,40 @@
+//
+//  RecipeTabView.swift
+//  Recipe App
+//
+//  Created by Jason Bullard on 1/12/22.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    
+    var body: some View {
+        
+        TabView {
+            
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+        
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
